1da2c146f8b18c4c4bae84bc889f2b3587dbf55e
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2017-09-13 10:59:50 +0200

    Clases y estructuras

---
+++ 02_ejemplos/06_tipos_personalizados/02_clases_estructuras/04_inicializador_memberwise.playground/Contents.swift
@@ -0,0 +1,10 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+struct Resolution {
+    var width = 0
+    var height = 0
+}
+
+let vga = Resolution(width: 640, height: 480)
---
+++ 02_ejemplos/06_tipos_personalizados/02_clases_estructuras/05_tipos_valor.playground/Contents.swift
@@ -0,0 +1,18 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+struct Resolution {
+    var width = 0
+    var height = 0
+}
+
+let hd = Resolution(width: 1920, height: 1080)
+
+var cinema = hd
+
+cinema.width = 2048
+
+print("cinema is now \(cinema.width) pixels wide")
+
+print("hd is still \(hd.width) pixels wide")
