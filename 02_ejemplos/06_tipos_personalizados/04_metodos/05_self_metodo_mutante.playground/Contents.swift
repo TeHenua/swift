feae15f9c640a8b3f48d9eacb344d6deea58d5e5
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2017-09-13 11:51:27 +0200

    Métodos y subíndices

---
+++ 02_ejemplos/06_tipos_personalizados/04_metodos/05_self_metodo_mutante.playground/Contents.swift
@@ -0,0 +1,32 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+struct Point {
+    var x = 0.0, y = 0.0
+    mutating func moveBy(x deltaX: Double, y deltaY: Double) {
+        self = Point(x: x + deltaX, y: y + deltaY)
+    }
+}
+
+enum TriStateSwitch {
+    case off, low, high
+    mutating func next() {
+        switch self {
+        case .off:
+            self = .low
+        case .low:
+            self = .high
+        case .high:
+            self = .off
+        }
+    }
+}
+
+var ovenLight = TriStateSwitch.low
+
+ovenLight.next()
+// ovenLight is now equal to .high
+
+ovenLight.next()
+// ovenLight is now equal to .off
