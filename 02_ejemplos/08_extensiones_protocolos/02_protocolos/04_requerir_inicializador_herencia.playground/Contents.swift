d6a3dd2ba361e28d5550bf7b117a3edbf76992a4
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2017-09-21 16:21:59 +0200

    Nuevos ejemplos

---
+++ 02_ejemplos/08_extensiones_protocolos/02_protocolos/04_requerir_inicializador_herencia.playground/Contents.swift
@@ -0,0 +1,20 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+protocol SomeProtocol {
+    init()
+}
+
+class SomeSuperClass {
+    init() {
+        // initializer implementation goes here
+    }
+}
+
+class SomeSubClass: SomeSuperClass, SomeProtocol {
+    // "required" from SomeProtocol conformance; "override" from SomeSuperClass
+    required override init() {
+        // initializer implementation goes here
+    }
+}
