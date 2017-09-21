d0cb518cbe84f5ac7e29c00fa2edb16ccc0ffa12
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2017-09-21 14:33:32 +0200

    Ciclo de vida de las instancias

---
+++ 02_ejemplos/07_ciclo_vida/01_inicializacion/09_inicializador_memberwise.playground/Contents.swift
@@ -0,0 +1,9 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+struct Size {
+    var width = 0.0, height = 0.0
+}
+
+let twoByTwo = Size(width: 2.0, height: 2.0)
---
+++ 02_ejemplos/07_ciclo_vida/03_arc/05_unowned_opcional_implicito.playground/Contents.swift
@@ -0,0 +1,25 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+class Country {
+    let name: String
+    var capitalCity: City!
+    init(name: String, capitalName: String) {
+        self.name = name
+        self.capitalCity = City(name: capitalName, country: self)
+    }
+}
+
+class City {
+    let name: String
+    unowned let country: Country
+    init(name: String, country: Country) {
+        self.name = name
+        self.country = country
+    }
+}
+
+var country = Country(name: "Canada", capitalName: "Ottawa")
+print("\(country.name)'s capital city is called \(country.capitalCity.name)")
+// Prints "Canada's capital city is called Ottawa"
